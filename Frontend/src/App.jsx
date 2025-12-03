bec945ae929a698eac54a31205bf67bc6b51a2f1
gopall-93 <gopallsharma93@gmail.com>
2025-12-03 13:06:56 +0530

    Implemented the Windows and it's functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Navbar,Welcome,Dock } from "@components";
+import gsap from "gsap";
+import { Navbar, Welcome, Dock } from "@components";
+import { Terminal } from "@windows";
+import { Draggable } from "gsap/Draggable";
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Welcome/>
-      <Dock/>
+      <Navbar />
+      <Welcome />
+      <Dock />
+      <Terminal />
     </main>
   );
 };
